a3e554a20d1ccd3e2b884800bd51b21c0fab3a94
Jcswindells <jcswindells@my.waketech.edu>
2025-04-11 10:31:09 -0400

    Add files via upload

---
+++ Swindells_Gradebook.js
@@ -0,0 +1,13 @@
+function fetchGradeData() {
+  console.log("fetchGradeData called");
+  // Stub: This function will fetch grade data from the database
+}
+
+function populateGradebook() {
+  console.log("populateGradebook called");
+  // Stub: This function will populate the gradebook table with fetched data
+}
+
+// Call the stubs for testing purposes
+fetchGradeData();
+populateGradebook();
